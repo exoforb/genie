3a0f8946a9595cf5d5d7b01395b79c45dc3e9389
exoforb <ekoadip12@gmail.com>
2025-05-05 00:46:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,18 +5,17 @@ RED='\033[0;31m'
 NC='\033[0m'
 local_ip=$(hostname -I | awk '{print $1}')
 echo -e "${GREEN}============================================================================${NC}"
-echo -e "${GREEN}=========================  ________  __  ____  =============================${NC}"
-echo -e "${GREEN}========================= |  ____\ \/ / / __ \ =============================${NC}"
-echo -e "${GREEN}========================= | |__   \  / | |  | |=============================${NC}"
-echo -e "${GREEN}========================= |  __|   / /  | |  | |=============================${NC}"
-echo -e "${GREEN}========================= | |____ / /__ | |__| |=============================${NC}"
-echo -e "${GREEN}========================= |______|____(_)____/ =============================${NC}"
-echo -e "${GREEN}============================================================================${NC}"
+echo -e "${GREEN}===================== _ _ === _ ==========  _ =============  _ =============${NC}"
+echo -e "${GREEN}=========== ___  __ _| | |__ (_)_   _  __ _| |__  _ __   ___| |_  ==========${NC}"
+echo -e "${GREEN}===========/ __|/ _` | | '_ \| | | | |/ _` | '_ \| '_ \ / _ \ __|  =========${NC}"
+echo -e "${GREEN}===========\__ \ (_| | | |_) | | |_| | (_| | | | | | | |  __/ |_  ==========${NC}"
+echo -e "${GREEN}===========|___/\__,_|_|_.__/|_|\__, |\__,_|_| |_|_| |_|\___|\__| ==========${NC}"
+echo -e "${GREEN}=============================== |___/  =====================================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
 echo -e "${GREEN}============================ Install GenieACS. =============================${NC}"
 echo -e "${GREEN}======================== NodeJS, MongoDB, GenieACS, ========================${NC}"
-echo -e "${GREEN}============================================================================${NC}"
+echo -e "${GREEN}===================== By SALBIYAH-NET. Info 081336128448 =====================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
 echo -e "${GREEN}Sebelum melanjutkan, silahkan baca terlebih dahulu. Apakah anda ingin melanjutkan? (y/n)${NC}"
 read confirmation
@@ -31,43 +30,66 @@ for ((i = 5; i >= 1; i--)); do
     echo "Lanjut Boskuh... $i. Tekan ctrl+c untuk membatalkan"
 done
 
-#!/bin/bash
+#MongoDB
+if ! sudo systemctl is-active --quiet mongod; then
+    curl -s \
+${url_install}\
+mongod.sh | \
+sudo bash
+else
+    echo -e "${GREEN}============================================================================${NC}"
+    echo -e "${GREEN}=================== mongodb sudah terinstall sebelumnya. ===================${NC}"
+    echo -e "${GREEN}============================================================================${NC}"
+fi
+sleep 3
+if ! sudo systemctl is-active --quiet mongod; then
+    sudo rm /tmp/install.sh
+    exit 1
+fi
 
-set -e
+#NodeJS Install
+check_node_version() {
+    if command -v node > /dev/null 2>&1; then
+        NODE_VERSION=$(node -v | cut -d 'v' -f 2)
+        NODE_MAJOR_VERSION=$(echo $NODE_VERSION | cut -d '.' -f 1)
+        NODE_MINOR_VERSION=$(echo $NODE_VERSION | cut -d '.' -f 2)
 
-GREEN='\033[0;32m'
-NC='\033[0m'
+        if [ "$NODE_MAJOR_VERSION" -lt 12 ] || { [ "$NODE_MAJOR_VERSION" -eq 12 ] && [ "$NODE_MINOR_VERSION" -lt 13 ]; } || [ "$NODE_MAJOR_VERSION" -gt 22 ]; then
+            return 1
+        else
+            return 0
+        fi
+    else
+        return 1
+    fi
+}
 
-# Ambil IP lokal
-local_ip=$(hostname -I | awk '{print $1}')
+if ! check_node_version; then
+    curl -s \
+${url_install}\
+nodejs.sh | \
+sudo bash
+else
+    NODE_VERSION=$(node -v | cut -d 'v' -f 2)
+    echo -e "${GREEN}============================================================================${NC}"
+    echo -e "${GREEN}============== NodeJS sudah terinstall versi ${NODE_VERSION}. ==============${NC}"
+    echo -e "${GREEN}========================= Lanjut install GenieACS ==========================${NC}"
+    echo -e "${GREEN}============================================================================${NC}"
 
-echo -e "${GREEN}================= STEP 1: Install Node.js v16 =================${NC}"
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
-apt-get install -y nodejs
+fi
+if ! check_node_version; then
+    sudo rm /tmp/install.sh
+    exit 1
+fi
 
-echo -e "${GREEN}Node.js version: $(node -v)${NC}"
-
-echo -e "${GREEN}================= STEP 2: Install MongoDB v6.0 =================${NC}"
-wget -qO - https://pgp.mongodb.com/server-6.0.asc | gpg --dearmor -o /usr/share/keyrings/mongodb-server-6.0.gpg
-
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-
-apt update
-apt install -y mongodb-org
-systemctl enable --now mongod
-
-echo -e "${GREEN}MongoDB version: $(mongod --version | grep "db version")${NC}"
-
-echo -e "${GREEN}================= STEP 3: Install GenieACS 1.2.13 ================${NC}"
-
-if ! systemctl is-active --quiet genieacs-cwmp; then
+#GenieACS
+if !  systemctl is-active --quiet genieacs-{cwmp,fs,ui,nbi}; then
+    echo -e "${GREEN}================== Menginstall genieACS CWMP, FS, NBI, UI ==================${NC}"
     npm install -g genieacs@1.2.13
-
     useradd --system --no-create-home --user-group genieacs || true
-
+    mkdir -p /opt/genieacs
     mkdir -p /opt/genieacs/ext
     chown genieacs:genieacs /opt/genieacs/ext
-
     cat << EOF > /opt/genieacs/genieacs.env
 GENIEACS_CWMP_ACCESS_LOG_FILE=/var/log/genieacs/genieacs-cwmp-access.log
 GENIEACS_NBI_ACCESS_LOG_FILE=/var/log/genieacs/genieacs-nbi-access.log
@@ -77,15 +99,13 @@ GENIEACS_DEBUG_FILE=/var/log/genieacs/genieacs-debug.yaml
 GENIEACS_EXT_DIR=/opt/genieacs/ext
 GENIEACS_UI_JWT_SECRET=secret
 EOF
-
     chown genieacs:genieacs /opt/genieacs/genieacs.env
-    chown -R genieacs:genieacs /opt/genieacs
+    chown genieacs. /opt/genieacs -R
     chmod 600 /opt/genieacs/genieacs.env
-
     mkdir -p /var/log/genieacs
-    chown genieacs:genieacs /var/log/genieacs
-
-    # Create service files
+    chown genieacs. /var/log/genieacs
+    # create systemd unit files
+## CWMP
     cat << EOF > /etc/systemd/system/genieacs-cwmp.service
 [Unit]
 Description=GenieACS CWMP
@@ -100,50 +120,53 @@ ExecStart=/usr/bin/genieacs-cwmp
 WantedBy=default.target
 EOF
 
+## NBI
     cat << EOF > /etc/systemd/system/genieacs-nbi.service
 [Unit]
 Description=GenieACS NBI
 After=network.target
-
+ 
 [Service]
 User=genieacs
 EnvironmentFile=/opt/genieacs/genieacs.env
 ExecStart=/usr/bin/genieacs-nbi
-
+ 
 [Install]
 WantedBy=default.target
 EOF
 
+## FS
     cat << EOF > /etc/systemd/system/genieacs-fs.service
 [Unit]
 Description=GenieACS FS
 After=network.target
-
+ 
 [Service]
 User=genieacs
 EnvironmentFile=/opt/genieacs/genieacs.env
 ExecStart=/usr/bin/genieacs-fs
-
+ 
 [Install]
 WantedBy=default.target
 EOF
 
+## UI
     cat << EOF > /etc/systemd/system/genieacs-ui.service
 [Unit]
 Description=GenieACS UI
 After=network.target
-
+ 
 [Service]
 User=genieacs
 EnvironmentFile=/opt/genieacs/genieacs.env
 ExecStart=/usr/bin/genieacs-ui
-
+ 
 [Install]
 WantedBy=default.target
 EOF
 
-    # Logrotate
-    cat << EOF > /etc/logrotate.d/genieacs
+# config logrotate
+ cat << EOF > /etc/logrotate.d/genieacs
 /var/log/genieacs/*.log /var/log/genieacs/*.yaml {
     daily
     rotate 30
@@ -152,33 +175,34 @@ EOF
     dateext
 }
 EOF
-
+    echo -e "${GREEN}========== Install APP GenieACS selesai... ==============${NC}"
     systemctl daemon-reload
     systemctl enable --now genieacs-{cwmp,fs,ui,nbi}
-
-    echo -e "${GREEN}================== Sukses install GenieACS ==================${NC}"
+    systemctl start genieacs-{cwmp,fs,ui,nbi}    
+    echo -e "${GREEN}================== Sukses genieACS CWMP, FS, NBI, UI ==================${NC}"
 else
-    echo -e "${GREEN}GenieACS sudah terinstall sebelumnya.${NC}"
+    echo -e "${GREEN}============================================================================${NC}"
+    echo -e "${GREEN}=================== GenieACS sudah terinstall sebelumnya. ==================${NC}"
 fi
 
+#Sukses
 echo -e "${GREEN}============================================================================${NC}"
 echo -e "${GREEN}========== GenieACS UI akses port 3000. : http://$local_ip:3000 ============${NC}"
-echo -e "${GREEN}============================================================================${NC}"
+echo -e "${GREEN}=================== Informasi: Whatsapp 081-336-128-448 ====================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
 
-# Restore parameter MongoDB
-echo -e "${GREEN}Sekarang install parameter default GenieACS. Lanjutkan? (y/n)${NC}"
+echo -e "${GREEN}Sekarang install parameter. Apakah anda ingin melanjutkan? (y/n)${NC}"
 read confirmation
 
 if [ "$confirmation" != "y" ]; then
     echo -e "${GREEN}Install dibatalkan..${NC}"
+    
     exit 1
 fi
-
 for ((i = 5; i >= 1; i--)); do
-    sleep 1
+	sleep 1
     echo "Lanjut Install Parameter $i. Tekan ctrl+c untuk membatalkan"
 done
 
 cd -
-sudo mongorestore --db=genieacs --drop new-genieacs
+sudo mongorestore --db=genieacs --drop genie
