735c6f695387f81096ab5666220bfecefa184639
exoforb <ekoadip12@gmail.com>
2025-05-05 08:33:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,9 +51,12 @@ sudo apt install -y nodejs
 echo -e "${GREEN}Node.js version: $(node -v)${NC}"
 
 echo -e "${GREEN}================= STEP 2: Install MongoDB v6.0 =================${NC}"
-wget -qO - https://pgp.mongodb.com/server-6.0.asc | gpg --dearmor -o /usr/share/keyrings/mongodb-server-6.0.gpg
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+wget -qO - https://www.mongodb.org/static/pgp/server-7.0.asc | gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-server-7.0.gpg > /dev/null
+sudo apt update
+sudo apt install -y mongodb-org
+
 
 apt update
 apt install -y mongodb-org
